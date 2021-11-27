c0cf4358ac44b32e558b23a7955a8d108cb404c3
Vincent J. Sinatra <vincentyoelsinatra2@gmail.com>
2021-11-27 21:31:29 +0700

    remove unused import and code cleanup

--- src/main/java/scrapper/Scrapper.java
+++ src/main/java/scrapper/Scrapper.java
@@ -5,60 +5,46 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import util.CsvFileUtils;
 
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Scrapper {
 
-    private static final String BASE_URL = "https://www.tokopedia.com/p/handphone-tablet/handphone?ob=100&page=1";
+    private static final String BASE_URL = "https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page=1";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WebClient client = configureClient();
-        try {
-            HtmlPage page = client.getPage(BASE_URL);
-            List<HtmlElement> items = page.getByXPath("//div[@class='css-bk6tzz e1nlzfl3']");
-            if (items.isEmpty()) {
-                System.out.println("No item found.");
+        HtmlPage page = client.getPage(BASE_URL);
+
+        List<HtmlElement> items = page.getByXPath("//div[@class='css-bk6tzz e1nlzfl3']");
+
+        if (items.isEmpty()) {
+            System.out.println("No item found.");
+        } else {
+            ArrayList<Item> itemList = new ArrayList<>();
+            for (HtmlElement htmlItem : items) {
+                HtmlAnchor itemAnchor = htmlItem.getFirstByXPath("//div[@class='css-bk6tzz e1nlzfl3']/a");
+                HtmlElement spanTitle = htmlItem.getFirstByXPath(".//span[@class='css-1bjwylw']");
+                HtmlElement spanPrice = htmlItem.getFirstByXPath(".//span[@class='css-o5uqvq']");
+                HtmlElement spanStoreName = (HtmlElement) htmlItem.getByXPath(".//span[@class='css-1kr22w3']").get(1);
+                List<HtmlElement> spanStar = htmlItem.getByXPath(".//img[@class='css-177n1u3']");
+
+                Item item = new Item();
+                item.setTitle(spanTitle == null ? "-" : spanTitle.getTextContent());
+                item.setImageUrl(itemAnchor == null ? "-" : itemAnchor.getHrefAttribute());
+                item.setPrice(spanPrice == null ? "0.0" : spanPrice.getTextContent());
+                item.setStoreName(spanStoreName == null ? "-" : spanStoreName.getTextContent());
+                item.setRating(spanStar == null ? 0 : spanStar.size());
+
+                itemList.add(item);
             }
-            else {
-                ArrayList<Item> itemList = new ArrayList<>();
-                for (HtmlElement htmlItem : items) {
-                    HtmlAnchor itemAnchor = htmlItem.getFirstByXPath("//div[@class='css-bk6tzz e1nlzfl3']/a");
-                    HtmlAnchor imageUrl = htmlItem.getFirstByXPath(".//img[@class='success fade']");
-                    HtmlElement spanTitle = htmlItem.getFirstByXPath(".//span[@class='css-1bjwylw']");
-                    HtmlElement spanPrice = htmlItem.getFirstByXPath(".//span[@class='css-o5uqvq']");
-                    HtmlElement spanStoreName = (HtmlElement) htmlItem.getByXPath(".//span[@class='css-1kr22w3']").get(1);
-                    List<HtmlElement> spanStar = htmlItem.getByXPath(".//img[@class='css-177n1u3']");
-
-                    Item item = new Item();
-                    item.setTitle(spanTitle == null ? "-" : spanTitle.getTextContent());
-                    item.setImageUrl(itemAnchor == null ? "-" : itemAnchor.getHrefAttribute());
-                    item.setPrice(spanPrice == null ? "0.0" : spanPrice.getTextContent());
-                    item.setStoreName(spanStoreName == null ? "-" : spanStoreName.getTextContent());
-                    item.setRating(spanStar == null ? 0 : spanStar.size());
-
-                    itemList.add(item);
-
-                }
-                ObjectMapper objectMapper = new ObjectMapper();
-                String jsonString = objectMapper.writeValueAsString(itemList);
-
-                System.out.println(jsonString);
-
-                CsvFileUtils.generateCsvFile(jsonString);
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonString = objectMapper.writeValueAsString(itemList);
+            CsvFileUtils.generateCsvFile(jsonString);
         }
     }
 
